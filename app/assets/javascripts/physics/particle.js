46caa8ee5a740ffb9087730f492211ee89de2982
Albin Svensson <albin.svensson@trialbee.com>
2015-09-08 01:39:44 +0200

    Added particle factory

--- app/assets/javascripts/physics/particle.js
+++ app/assets/javascripts/physics/particle.js
@@ -38,3 +38,35 @@ function Particle(x, y, mass, world){
     this.world.canvas.appendChild(svg);
   }
 }
+
+function ParticleFactory(world, maxMass){
+  this.maxMass = maxMass;
+  this.world = world;
+  this.build = function(numberOfParticles){
+    var particles = new Array();
+    for (var i = 0; i < numberOfParticles; i++) {
+      var particle = this.buildParticle();
+      particle.color = this.getRandomColor();
+      particles.push(particle);  
+    }
+    return particles;
+  }
+  this.buildParticle = function(){
+    return new Particle(
+        this.randomInteger(this.world.sizeX),
+        this.randomInteger(this.world.sizeY),
+        this.randomInteger(this.maxMass) + 1,
+        world);
+  }
+  this.randomInteger = function(max){
+    return Math.floor(Math.random() * max);
+  }
+  this.getRandomColor = function() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+}
--- app/assets/javascripts/physics/physics.js
+++ app/assets/javascripts/physics/physics.js
@@ -1,32 +1,31 @@
 $(function(){
   var width = 2000;
   var height = 2000;
-  var numberOfParticles = 100;
   world = new World(width, height, 'world');
-  var mouseStrength = 100;
-  var dragStrength = 50;
-  var gravity = new GravityForce(new Vector(0, 1), 0.2);
-  var friction = new FrictionForce(0.9);
+  
+  var numberOfParticles = 40;
+  var mouseStrength     = 100;
+  var dragStrength      = 50;
+  var keyStrength       = 10;
+  var gravity = new GravityForce(new Vector(0, 1), 0.3);
+  var friction = new FrictionForce(0.2);
 
-  var particles = new Array();
-  for (var i = 0; i < numberOfParticles; i++) {
-    var particle = new Particle(randomInteger(width), randomInteger(height), randomInteger(20) + 1, world);
-    particle.color = getRandomColor();
-    particles.push(particle);  
-  }
+  var pf = new ParticleFactory(world, 20);
+  var particles = pf.build(numberOfParticles);
+
   function updateWorld(){
     var mouseDrag = new DragForce(getRelativeMousePosition(world), mouseStrength);
-    var keyForce = new PushForce(keyPush(), 10);
+    var keyForce = new PushForce(keyPush(), keyStrength);
     var dragPoint = new DragForce(fixedDragPoint(), dragStrength);
 
     world.clear();
     for (my_particle in particles) {
       var particle = particles[my_particle];
       var forces   = [
-        gravity.compute(particle),
+        // gravity.compute(particle),
         mouseDrag.compute(particle),
-        dragPoint.compute(particle),
-        keyForce.compute(particle),
+        // dragPoint.compute(particle),
+        // keyForce.compute(particle),
         friction.compute(particle)
       ];
       particle.update(forces);
@@ -47,33 +46,30 @@ document.onmousemove = function(e){
 }
 
 function getRelativeMousePosition(canvas) {
-  if(mousePosition){
-    return mousePosition;
-  } else {
+  if(!mousePosition)
     mousePosition = new Vector(1000, 1000);
-    return mousePosition;
-  } 
+  return mousePosition;
 }
 document.onkeydown = function(e) {
-  setActiveKey(e.keyCode);
+  keyForce = activeDirection(e.keyCode);
 }
 document.onkeyup = function(e) {
   keyForce = new Vector(0, 0);
 }
 
 var keyForce = false
-function setActiveKey(keyCode){
+function activeDirection(keyCode){
   if (keyCode == '38') {
-    keyForce = new Vector(0, -1);
+    return new Vector(0, -1);
   }
   else if (keyCode == '40') {
-    keyForce = new Vector(0, 1);
+    return new Vector(0, 1);
   }
   else if (keyCode == '37') {
-    keyForce = new Vector(-1, 0);
+    return new Vector(-1, 0);
   }
   else if (keyCode == '39') {
-    keyForce = new Vector(1, 0);
+    return new Vector(1, 0);
   }
 }
 
@@ -89,17 +85,4 @@ function fixedDragPoint() {
     dragPoint = new Vector(1000, 1000);
   dragPoint = dragPoint.add(new Vector(Math.random()-0.5, Math.random()-0.5));
   return dragPoint;
-}
-
-function randomInteger(max){
-  return Math.floor(Math.random() * max);
-}
-
-function getRandomColor() {
-  var letters = '0123456789ABCDEF'.split('');
-  var color = '#';
-  for (var i = 0; i < 6; i++ ) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
 }
